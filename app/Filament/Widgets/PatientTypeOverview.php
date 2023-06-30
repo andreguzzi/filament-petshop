18074f66b6fefd50e60e1841ee4e7fcb55e76af1
andreguzzi <andre_guzzi@hotmail.com>
2023-06-30 10:08:20 -0300

    first commit

---
+++ app/Filament/Widgets/PatientTypeOverview.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Patient;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class PatientTypeOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            Card::make('Cats', Patient::query()->where('type', 'cat')->count()),
+            Card::make('Dogs', Patient::query()->where('type', 'dog')->count()),
+            Card::make('Rabbits', Patient::query()->where('type', 'rabbit')->count()),
+            Card::make('Hamsters', Patient::query()->where('type', 'hamster')->count()),
+        ];
+    }
+}
